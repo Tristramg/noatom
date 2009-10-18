959dd8a4bfca793adfd618c2e68732b8da30382c
Tristram Gräbener <tristramg@gmail.com>
2009-10-19 00:08:39 +0200

    Parsing type1 powerplants and small code simplification

--- parse.cc
+++ parse.cc
@@ -10,6 +10,29 @@ typedef file_iterator<char>   iterator_t;
 typedef scanner<iterator_t>     scanner_t;
 typedef rule<scanner_t>         rule_t;
 
+struct Powerplant_t1
+{
+    std::string name;
+    int index;
+    std::deque<std::vector<float> > pmin;
+    std::deque<std::vector<float> > pmax;
+    std::deque<std::vector<float> > cost;
+
+    Powerplant_t1(size_t scenarios, size_t timesteps) :
+        pmin(scenarios),
+        pmax(scenarios),
+        cost(scenarios)
+    {
+        for(size_t i=0; i < scenarios; i++)
+        {
+            pmin[i].reserve(timesteps);
+            pmax[i].reserve(timesteps);
+            cost[i].reserve(timesteps);
+        }
+    }
+
+};
+
 int main(int argc, char ** argv)
 {
     if(argc != 2)
@@ -42,65 +65,40 @@ int main(int argc, char ** argv)
     int constraint19 = 0;
     int constraint20 = 0;
     int constraint21 = 0;
-
-    rule_t header_r = str_p("begin main\n")
-        >> str_p("timesteps ") >> int_p[assign_a(timesteps)] >> '\n'
-        >> str_p("weeks ") >> int_p[assign_a(weeks)] >> '\n'
-        >> str_p("campaigns ") >> int_p[assign_a(campaigns)] >> '\n'
-        >> str_p("scenario ") >> int_p[assign_a(scenario)] >> '\n'
-        >> str_p("epsilon ") >> real_p[assign_a(epsilon)] >> '\n'
-        >> str_p("powerplant1 ") >> int_p[assign_a(powerplant1)] >> '\n'
-        >> str_p("powerplant2 ") >> int_p[assign_a(powerplant2)] >> '\n'
-        >> str_p("constraint13 ") >> int_p[assign_a(constraint13)] >> '\n'
-        >> str_p("constraint14 ") >> int_p[assign_a(constraint14)] >> '\n'
-        >> str_p("constraint15 ") >> int_p[assign_a(constraint15)] >> '\n'
-        >> str_p("constraint16 ") >> int_p[assign_a(constraint16)] >> '\n'
-        >> str_p("constraint17 ") >> int_p[assign_a(constraint17)] >> '\n'
-        >> str_p("constraint18 ") >> int_p[assign_a(constraint18)] >> '\n'
-        >> str_p("constraint19 ") >> int_p[assign_a(constraint19)] >> '\n'
-        >> str_p("constraint20 ") >> int_p[assign_a(constraint20)] >> '\n'
-        >> str_p("constraint21 ") >> int_p[assign_a(constraint21)] >> '\n';
-
-    std::cout << "Parsing header: " << std::flush;
-    parse_info<iterator_t> info = parse(first, last, header_r);
-    if(!info.hit)
-    {
-        std::cout << "Fail" << std::endl;
-        return 1;
-    }
-    else
-    {
-        std::cout << "Ok" << std::endl;
-    }
-
-
-    std::cout << "Parsing durations: " << std::flush;
     std::deque<int> durations;
-    rule_t durations_r = "durations" >> *(' '|int_p[push_back_a(durations)]) >> '\n';
 
-    info = parse(info.stop, last, durations_r);
-     if(!info.hit)
-    {
-        std::cout << "Fail" << std::endl;
-        return 1;
-    }
-     else if(durations.size() != timesteps)
-     {
-         std::cout << "wrong number of durations" << std::endl;
-         return 1;
-     }
-    else
-    {
-        std::cout << "Ok" << std::endl;
-    }
+    rule_t header_r = "begin main" >> eol_p
+        >> "timesteps " >> int_p[assign_a(timesteps)] >> eol_p
+        >> "weeks " >> int_p[assign_a(weeks)] >> eol_p
+        >> "campaigns " >> int_p[assign_a(campaigns)] >> eol_p
+        >> "scenario " >> int_p[assign_a(scenario)] >> eol_p
+        >> "epsilon " >> real_p[assign_a(epsilon)] >> eol_p
+        >> "powerplant1 " >> int_p[assign_a(powerplant1)] >> eol_p
+        >> "powerplant2 " >> int_p[assign_a(powerplant2)] >> eol_p
+        >> "constraint13 " >> int_p[assign_a(constraint13)] >> eol_p
+        >> "constraint14 " >> int_p[assign_a(constraint14)] >> eol_p
+        >> "constraint15 " >> int_p[assign_a(constraint15)] >> eol_p
+        >> "constraint16 " >> int_p[assign_a(constraint16)] >> eol_p
+        >> "constraint17 " >> int_p[assign_a(constraint17)] >> eol_p
+        >> "constraint18 " >> int_p[assign_a(constraint18)] >> eol_p
+        >> "constraint19 " >> int_p[assign_a(constraint19)] >> eol_p
+        >> "constraint20 " >> int_p[assign_a(constraint20)] >> eol_p
+        >> "constraint21 " >> int_p[assign_a(constraint21)] >> eol_p
+        >> "durations" >> *(' '|int_p[push_back_a(durations)]) >> eol_p;
+
+
+    std::cout << "Parsing header: " << std::flush; parse_info<iterator_t> info
+        = parse(first, last, header_r); if(!info.hit) { std::cout << "Fail" <<
+            std::endl; return 1; } else if(durations.size() != timesteps) {
+                std::cout << "wrong number of durations" << std::endl; return
+                    1; } else { std::cout << "Ok" << std::endl; }
+
 
     std::cout << "Parsing demand: " << std::flush;
-    std::deque<std::deque<float> > demand(scenario);
+    std::vector<std::vector<float> > demand(scenario);
 
-    for(int i=0; i < scenario; i++)
-    {
-    
-        rule_t demand_r = "demand" >> *(' '|real_p[push_back_a(demand[i])]) >> '\n';
+    for(int i=0; i < scenario; i++) { demand[i].reserve(timesteps);
+        rule_t demand_r = "demand" >> *(' '|real_p[push_back_a(demand[i])]) >> eol_p;
         info = parse(info.stop, last, demand_r);
         if(!info.hit)
         {
@@ -115,8 +113,56 @@ int main(int argc, char ** argv)
     }
     std::cout << "Ok" << std::endl;
 
-    info = parse(info.stop, last, str_p("end main\n"));
+    info = parse(info.stop, last, "end main" >> eol_p);
     std::cout << "Main section done" << std::endl << std::endl;
+
+    std::cout << "Parsing Powerplant type1: " << std::flush;
+    std::deque<Powerplant_t1> plants1 (powerplant1, Powerplant_t1(scenario, timesteps));
+    for(int i=0; i < powerplant1; i++)
+    {
+        rule_t type1_r = "begin powerplant" >> eol_p
+            >> "name " >> (*~space_p)[assign_a(plants1[i].name)] >> eol_p
+            >> "type 1" >> eol_p
+            >> "index " >> int_p[assign_a(plants1[i].index)] >> eol_p
+            >> "scenario " >> int_p >> eol_p
+            >> "timesteps " >> int_p >> eol_p;
+
+        info = parse(info.stop, last, type1_r);
+        if(!info.hit)
+        {
+            std::cout << "Fail header of powerplant #" << i << std::endl;
+            return 1;
+        }
+
+        for(int s=0; s < scenario; s++)
+        {
+
+            rule_t data_r = "pmin" >> *(' '|real_p[push_back_a(plants1[i].pmin[s])]) >> eol_p
+                >> "pmax" >> *(' '|real_p[push_back_a(plants1[i].pmax[s])]) >> eol_p
+                >> "cost" >> *(' '|real_p[push_back_a(plants1[i].cost[s])]) >> eol_p;
+            info = parse(info.stop, last, data_r);
+            if(!info.hit)
+            {
+                std::cout << "Fail data of powerplant #" << i << std::endl;
+                return 1;
+            }
+            else if(plants1[i].pmin[s].size() != timesteps
+                    || plants1[i].pmax[s].size() != timesteps
+                    || plants1[i].cost[s].size() != timesteps)
+            {
+                std::cout << "Wrong number of data of powerplant #" << i << " and scenario #" << s << std::endl;
+                return 1;
+            }
+        }
+        info = parse(info.stop, last, "end powerplant" >> eol_p);
+        if(!info.hit)
+        {
+            std::cout << "Missing \"end powerplant\" of powerplant #" << i << std::endl;
+            return 1;
+        }
+
+    }
+    std::cout << "Ok" << std::endl;
 
     return 0;
 }
